f26e8813b9a2afd7933c2cc34c97fe78770f7308
Alex <str1t3r@gmail.com>
2015-03-03 14:47:00 +0300

    jacobi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Main {
 
     final static int N = 3;
@@ -23,22 +21,53 @@ public class Main {
             }
             res[i] = temp / system[i][i];
         }
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                System.out.print(system[i][j] + " ");
-            }
-            System.out.println(free[i]);
-        }
         return res;
+    }
+
+    public static double[] jacobi(double[][] a, double[] f) {
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; j < a.length;++j) {
+                if (i != j) {
+                    a[i][j] /= -a[i][i];
+                }
+            }
+            f[i] /= a[i][i];
+        }
+
+        double[] currentX = new double[N];
+        double[] nextX = new double[N];
+        double cond = 10;
+        final double EPS = 0.0000001;
+        while (cond > EPS) {
+            for (int i = 0; i < N; ++i) {
+                double temp = f[i];
+                for (int j = 0; j < N; ++j) {
+                    if (i != j) {
+                        temp += a[i][j] * currentX[j];
+                    }
+                }
+                nextX[i] = temp;
+            }
+            double max = Double.MIN_VALUE;
+            for (int i = 0; i < N; ++i) {
+                if (max < Math.abs(currentX[i] - nextX[i])) {
+                    max = Math.abs(currentX[i] - nextX[i]);
+                }
+            }
+            cond = max;
+            System.arraycopy(nextX, 0, currentX, 0, currentX.length);
+        }
+        return nextX;
     }
 
     public static void main(String[] args) {
 
-        double[][] matrix = {{8, 7, 3}, {-7, -4, -4}, {-6, 5, -4}};
-        double[] free = {18, -11, -15};
-        double[] res = gauss(matrix, free);
+        double[][] matrix = {{8, 7, 3}, {3, 5, 1}, {3, -2, 10}};
+        double[] free = {10, 5, 4};
+        double[] resJacobi = jacobi(matrix, free);
+        double[] resGauss = gauss(matrix, free);
         for (int i = 0; i < N; ++i) {
-            System.out.println(res[i]);
+            System.out.println(resJacobi[i] + " " + resGauss[i]);
         }
     }
 }
